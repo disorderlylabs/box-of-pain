d6239fc51fd188efc0de8d528fc9d62a422d7289
Daniel Bittman <danielbittman1@gmail.com>
2018-01-16 21:07:29 -0800

    Updated qs.py for python3.

--- quorum_server.py
+++ quorum_server.py
@@ -1,8 +1,10 @@
+#!/usr/bin/env python
 import socket, sys
 
 s = socket.socket()
+s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 host = socket.gethostname()
-print "host is " + str(host)
+print("host is " + str(host))
 port = 1234
 s.bind(("127.0.0.1",port))
 s.listen(5)
@@ -11,18 +13,18 @@ s.listen(5)
 votes = 0
 
 while True:
-    print "call accept"
+    print("call accept")
     c, addr = s.accept()
     print("Connection accepted from " + repr(addr[1]))
     msg = c.recv(128)
-    print "msg " + msg
-    c.send("Server approved connection\n")
-    print repr(addr[1]) + ": " + c.recv(1026)
+    print("msg " + str(msg))
+    c.send(b"Server approved connection\n")
+    print(str(repr(addr[1])) + ": " + str(c.recv(1026)))
 
     votes += 1
 
     c.close()
 
     if votes >= 3:
-	print "OUT"
-	sys.exit(0)
+        print("OUT")
+        sys.exit(0)
