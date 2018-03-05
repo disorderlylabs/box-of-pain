c2d27043eda8872ebb152fc9b07c68bd1fa26336
Boris Gorshenev <bgorshen@ucsc.edu>
2018-03-05 10:53:28 -0800

    added clone.cpp to repository

---
+++ sysimp/clone.cpp
@@ -0,0 +1,37 @@
+#include <sys.h>
+#include <tracee.h>
+#include <unordered_map>
+
+extern std::vector<struct thread_tr *> thread_list;
+extern std::unordered_map<int, struct thread_tr *> traces;
+
+void Sysclone::start() {}
+
+
+
+void Sysclone::finish() {
+	int newtid = retval;
+	//ptrace(PTRACE_GETEVENTMSG, fromtid, 0, &newtid);
+
+	fprintf(stderr, "[%d]: clone() creates new thread with tid: %d\n",
+			thread->id, newtid);
+
+	//Create a tracee object for the new thread
+	struct thread_tr *tr = new thread_tr();
+	tr->sysnum = -1; //we're not in a syscall to start.
+	tr->syscall_rip = -1;
+	tr->shared_page = 0;
+	tr->sp_mark = 0;
+	tr->syscall = NULL;
+	tr->proc = thread->proc;
+	tr->id = thread->id;
+	tr->tid = newtid;
+	traces[tr->tid] = tr;
+	thread_list.push_back(tr);
+
+	//Tell the new thread to run
+	ptrace(PTRACE_SYSCALL, newtid, 0, 0);
+	if(errno != 0) { perror("ptrace SYSCALL"); }
+}
+
+
