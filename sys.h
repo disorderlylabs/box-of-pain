0c92cd2701f361174e8c81b59ae22aadd14fa1b1
palvaro <palvaro@ucsc.edu>
2019-02-02 19:56:49 +0000

    push daniel's changes from my guest vm

--- sys.h
+++ sys.h
@@ -45,7 +45,7 @@ class event
   public:
 	Syscall *sc;
 	bool entry, pending;
-	int err_code;
+	int err_code=0;
 	int trid, uuid;
 	/* this vector lists the extra partial-order "parents" of this event */
 	std::vector<event *> extra_parents;
