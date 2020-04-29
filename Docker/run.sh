037e21503d47fac989d6b3fdbcfc6b953542dd25
Nick <nzandber@ucsc.edu>
2020-04-28 18:32:54 -0700

    cosmetic modifications

--- Docker/run.sh
+++ Docker/run.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-docker run --cap-add=SYS_PTRACE --rm -it --mount type=bind,source=$(pwd)/tmp,target=/tmp --entrypoint ./painbox painbox -e ./client -e ./server
+docker run \
+    --cap-add=SYS_PTRACE \
+    --rm \
+    -it \
+    --mount type=bind,source=$(pwd)/tmp,target=/tmp \
+    --entrypoint \
+    ./painbox painbox -e ./client -e ./server
